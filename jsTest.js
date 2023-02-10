62d0e082255bcc66753e0760d3f7f728e38e54f6
kenneally15 <kenneally15@yahoo.com>
2023-02-10 17:40:56 -0500

    Initial commit

---
+++ jsTest.js
@@ -0,0 +1,7 @@
+console.log('---------');
+console.log('---------');
+console.log('Hello World');
+console.log('This is a test Javascript script');
+console.log('   ---------   ----------  -----');
+console.log('-------');
+
---
+++ test.py
@@ -0,0 +1,5 @@
+'''This is a test file'''
+
+print("Hello, world! This is a test file")
+print("Second line")
+print("Third line")
